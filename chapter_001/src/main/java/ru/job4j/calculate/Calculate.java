7978b45542c7028d69c0d111740af26d47f98916
Michael Dedun <michael.dedun@mail.ru>
2020-02-26 14:59:22 +0300

    Задание [#240882]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Michael dedun (michae.dedun@mail.ru)
+ * @version 1
+ * @since 26.02.2020
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Michael dedun (michae.dedun@mail.ru)
+ * @version 1
+ * @since 26.02.2020
+ */
+package ru.job4j.calculate;
