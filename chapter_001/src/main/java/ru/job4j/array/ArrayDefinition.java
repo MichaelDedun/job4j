327cf1db4db14f910fd1ad91511d33f64ca16c7e
Michael Dedun <michael.dedun@mail.ru>
2020-02-27 12:57:10 +0300

    Задание [#240816]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,5 +7,17 @@ public class ArrayDefinition {
     String[] surnames = new String[100500];
 
     float[] prices = new float[40];
-    
+
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Michael Dedun";
+        names[2] = "Andrei Hincu";
+        names[3] = "Rail Shamsemuhametov";
+
+        for (String name : names)
+            System.out.println(name);
+        
+    }
+
 }
