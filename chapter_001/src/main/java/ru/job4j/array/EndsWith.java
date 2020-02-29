b601f49b10de69d09efe46d65d987d44ce902462
Michael Dedun <michael.dedun@mail.ru>
2020-02-29 19:15:49 +0300

    Исправлено [#240808]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -5,7 +5,10 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = false;
         for (int i = 0; i < post.length; i++) {
-            return word[word.length - 1 - i] == post[post.length - 1 - i];
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+                result = false;
+                break;
+            } else result = true;
         }
         return result;
     }
