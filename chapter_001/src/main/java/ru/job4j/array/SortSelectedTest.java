cb2d49a7e325b5e589010561c65e82e0b229c9c9
Michael Dedun <michael.dedun@mail.ru>
2020-02-29 20:10:00 +0300

    Задание #240810]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSort() {
+        int[] input = new int[]{3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void SortWhenArrayHasThreeElements() {
+        int[] input = new int[]{3, 2, 2};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[]{2, 2, 3};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void SortWhenArrayHasFiveElements() {
+        int[] input = new int[]{Integer.MAX_VALUE, -1, 0, 2, 3};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[]{-1, 0, 2, 3, Integer.MAX_VALUE};
+        assertThat(result, is(expect));
+    }
+
+}
