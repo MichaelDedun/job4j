c4a87a5af051ac36b07f3f77986310c590b4f022
Michael Dedun <michael.dedun@mail.ru>
2020-03-10 13:24:10 +0300

    Задание [#240967]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,19 +1,23 @@
 package ru.job4j.condition;
 
 public class Point {
+    private int x;
+    private int y;
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        double result1 = Point.distance(1, 2, 3, 4);
-        double result2 = Point.distance(2, 10, 4, 15);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result (1, 3) to (2, 4) " + result1);
-        System.out.println("result (2, 4) to (10, 15) " + result2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 
 }
