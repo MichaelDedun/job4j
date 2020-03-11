9550ae61bbfe0c5401f08ac5124458a6fbe1ce88
Michael Dedun <michael.dedun@mail.ru>
2020-03-11 21:08:29 +0300

    Задание [#240996]

--- chapter_002/src/main/java/ru/job4j/inheritance/JsonReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -1,12 +1,13 @@
 package ru.job4j.inheritance;
 
-public class JsonReport extends TextReport{
+public class JsonReport extends TextReport {
 
+    @SuppressWarnings("checkstyle:OperatorWrap")
     @Override
     public String generate(String name, String body) {
         return "{" +
-                    "\n\tname: " + name +
-                    "\n\tbody: " + body +
+                "\n\tname: " + name +
+                "\n\tbody: " + body +
                 "\n}";
     }
 
