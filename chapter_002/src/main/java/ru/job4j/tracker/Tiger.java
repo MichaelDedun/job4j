e79d09f2b54045bb581074e879c74883a3b02b40
Michael Dedun <michael.dedun@mail.ru>
2020-03-11 15:10:14 +0300

    Задание [#240983]

--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -2,13 +2,13 @@ package ru.job4j.tracker;
 
 public class Tiger extends Predator {
 
-    public Tiger() {
-        super();
+    public Tiger(String name) {
+        super(name);
         System.out.println("Tiger");
     }
 
     public static void main(String[] args) {
-        Tiger tiger = new Tiger();
+        Tiger tiger = new Tiger("Шерхан");
     }
-    
+
 }
