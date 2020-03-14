fc44fdd896dc102fbcd580d4f667e39ccaa1c667
Michael Dedun <michael.dedun@mail.ru>
2020-03-14 21:20:14 +0300

    Задание [#240940]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -4,22 +4,74 @@ import java.util.Scanner;
 
 public class Matches {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int matches = 11;
-        while (matches != 0) {
-            System.out.println("Ходит первый игрок, возьмите количество спичек от 1 до 3");
-            int select = Integer.valueOf(input.nextLine());
-            matches -= select;
-            System.out.println("Спичек осталось: " + matches);
-            System.out.println("Ходит второй игрок, возьмите количество спичек от 1 до 3");
-            select = Integer.valueOf(input.nextLine());
-            matches -= select;
-            System.out.println("Спичек осталось: " + matches);
-            if (matches <= 0) {
-                System.out.println("Победа");
-            }
+    static class Player {
+        private String name;
+
+        public Player(String name) {
+            this.name = name;
         }
+
+        public int takeMatches() {
+            Scanner input = new Scanner(System.in);
+            return Integer.parseInt(input.nextLine());
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    private static int matches = 11;
+    private static Player[] players;
+
+    public static void main(String[] args) {
+        int select, current = 0;
+        players = initPlayers(2);
+        Player lastPlayer = players[0];
+        while (!isFinished()) {
+            do {
+                System.out.println(players[current].getName() + " , возьмите количество спичек от 1 до 3");
+                select = players[current].takeMatches();
+            } while (!checkAmount(select));
+            matches -= select;
+            printAmount();
+            lastPlayer = players[current];
+            current = getNextPlayerIndex(players, current);
+        }
+        System.out.println(lastPlayer.getName() + " won");
+    }
+
+    private static int getNextPlayerIndex(Player[] players, int current) {
+        int next = current + 1;
+        if (next == players.length) {
+            next = 0;
+        }
+        return next;
+    }
+
+    private static Player[] initPlayers(int num) {
+        Player[] players = new Player[num];
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player("Player [" + (i + 1) + "]");
+        }
+        return players;
+    }
+
+    private static boolean checkAmount(int amount) {
+        boolean result = true;
+        if (amount < 1 || amount > 3 || amount > matches) {
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean isFinished() {
+        return matches == 0;
+    }
+
+    private static void printAmount() {
+        System.out.println("Спичек осталось: " + matches);
     }
 
 }
