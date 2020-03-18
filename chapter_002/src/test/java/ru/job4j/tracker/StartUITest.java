26d2e6c440f51ca149e4513c5fd4b58086858a64
Michael Dedun <michael.dedun@mail.ru>
2020-03-18 18:18:40 +0300

    Задание [#240937]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -56,4 +56,13 @@ public class StartUITest {
         assertNull(tracker.findById(item.getId()));
     }
 
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[]{"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
+        assertThat(action.isCall(), is(true));
+    }
 }
