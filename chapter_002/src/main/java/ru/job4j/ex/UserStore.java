e9197a5972e39394b85ab859b06f5250823166e2
Michael Dedun <michael.dedun@mail.ru>
2020-03-19 14:00:17 +0300

    Задание [#240961]

---
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,44 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User result = null;
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                result = user;
+            }
+        }
+        if (result == null) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        return result;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
+            throw new UserInvalidException("пользователь не валидный");
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true), new User("Michael Dedun", false)
+        };
+
+        try {
+            User user = findUser(users, "Michael Dedun");
+            validate(user);
+        } catch (UserInvalidException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
