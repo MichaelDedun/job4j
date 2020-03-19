05604ad450675adcd8f7763a0ca9f8d59345f7e0
Michael Dedun <michael.dedun@mail.ru>
2020-03-19 15:07:53 +0300

    Задание [#240962]

---
+++ chapter_002/src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class FactRec {
+
+    public static int calc(int n) {
+        int result = 0;
+        if (n == 0) {
+            result = 1;
+        } else {
+            result = calc(n - 1) * n;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rsl = calc(3);
+        System.out.println(rsl);
+    }
+
+}
