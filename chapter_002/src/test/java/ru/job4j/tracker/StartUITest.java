f36f6311104e951d04db34eef9a7a6e71961fe0e
Michael Dedun <michael.dedun@mail.ru>
2020-03-20 02:18:17 +0300

    Задание [#240938]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -88,4 +88,39 @@ public class StartUITest {
         assertThat(action.isCall(), is(true));
     }
 
+    @Test
+    public void whenCheckOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindAllAction act = new FindAllAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("=== Show all Item's ====")
+                .add("Имя:" + item.getName() + " Айди: " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+    @Test
+    public void findByNameExecute() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindByNameAction act = new FindByNameAction();
+        act.execute(new StubInput(new String[] {"fix bug"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Имя: " + item.getName() + "Айди: " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
 }
