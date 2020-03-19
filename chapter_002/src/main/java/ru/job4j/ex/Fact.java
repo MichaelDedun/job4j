09cd4c17f69a63f8576ac396f446514bb7d454b6
Michael Dedun <michael.dedun@mail.ru>
2020-03-19 12:43:53 +0300

    Задание [#240958]

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+
+    public static void main(String[] args) {
+        System.out.println("Factorial: " + calc(0));
+    }
+
+    public static int calc(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+}
