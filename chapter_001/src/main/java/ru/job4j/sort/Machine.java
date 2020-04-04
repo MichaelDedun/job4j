c6b0583864765b7140d5acf3fe023abb7f6e32ea
Michael Dedun <michael.dedun@mail.ru>
2020-04-04 14:22:46 +0300

    Задание [#240893]

--- chapter_001/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public class Machine {
     @SuppressWarnings("checkstyle:MemberName")
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int surrender = money - price;
-        for (int i = 0; i < COINS.length; i++) {
+        for (int i = 0; i < coins.length; i++) {
             while (surrender != 0) {
-                if (surrender - COINS[i] < 0) {
+                if (surrender - coins[i] < 0) {
                     break;
                 }
-                surrender = surrender - COINS[i];
-                rsl[i] = COINS[i];
+                surrender = surrender - coins[i];
+                rsl[i] = coins[i];
                 size++;
             }
         }
