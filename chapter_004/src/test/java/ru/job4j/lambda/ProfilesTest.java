a67bf8fd554b99baba6ded21c9c4087abfd3ebf6
Michael Dedun <michael.dedun@mail.ru>
2020-04-08 16:38:08 +0300

    Задание [#240766]

---
+++ chapter_004/src/test/java/ru/job4j/lambda/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProfilesTest {
+
+    @Test
+    public void testProfileGetAddress() {
+        Profiles profiles = new Profiles();
+        List<Profile> profilesList = Arrays.asList(
+                new Profile(new Address("Omsk", "ordzhonikidze", 83, 70)),
+                new Profile(new Address("Moscow", "pobeda", 74, 28))
+        );
+        List<Address> result = profiles.collect(profilesList);
+        List<Address> expected = Arrays.asList(
+                new Address("Omsk", "ordzhonikidze", 83, 70),
+                new Address("Moscow", "pobeda", 74, 28)
+        );
+        Assert.assertEquals(result, expected);
+    }
+
+}
