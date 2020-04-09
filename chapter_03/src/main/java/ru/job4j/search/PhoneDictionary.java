e75fcab6552ab227799313ae7b20ec3a0ecfbec8
Michael Dedun <michael.dedun@mail.ru>
2020-04-09 17:18:24 +0300

    Задание [#240757]

--- chapter_03/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_03/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -16,12 +18,14 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> byName = person -> person.getName() != null;
+        Predicate<Person> bySurname = person -> person.getSurname() != null;
+        Predicate<Person> byPhone = person -> person.getPhone() != null;
+        Predicate<Person> byAddress = person -> person.getAddress() != null;
+        Predicate<Person> combine = byName.or(bySurname).or(byPhone).or(byAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getSurname().equals(key)
-                    || person.getAddress().equals(key)
-                    || person.getName().equals(key)
-                    || person.getPhone().equals(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
