ca452f83c1efed5c288587aa6b5dd0aa5c52a2d7
Michael Dedun <michael.dedun@mail.ru>
2020-04-11 14:49:40 +0300

    Задание [#240896]

--- chapter_03/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_03/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,18 +6,10 @@ public class DepDescComp implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
         String[] left = o1.split("/");
         String[] right = o2.split("/");
-        if (left[0].compareTo(right[0]) == 0) {
-            rsl = Integer.compare(left.length, right.length);
-            if (rsl == 0) {
-                rsl = left[left.length - 1].compareTo(right[right.length - 1]);
-            }
-        } else {
-            rsl = right[0].compareTo(left[0]);
-        }
-        return rsl;
+        int rsl = right[0].compareTo(left[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 
 }
