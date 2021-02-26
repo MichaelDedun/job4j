2bb559670360d6740d6c654690a700b3c402b8d3
Michael Dedun <michael.dedun@mail.ru>
2021-02-26 12:25:24 +0300

    Что такое DI?[6876#317812]

---
+++ chapter_002/src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,32 @@
+package ru.job4j.di;
+
+import ru.job4j.tracker.ConsoleInput;
+
+public class StartUI {
+    private Store store;
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+
+    public String askStr(String question) {
+       return consoleInput.askStr(question);
+    }
+
+    public Integer askInt(String question) {
+        return consoleInput.askInt(question);
+    }
+
+}
